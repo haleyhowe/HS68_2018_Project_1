244732d6a26e73495bd3f4af06e5283aef85fc5b
Nikita Thomas <nikita1103@gmail.com>
2018-07-17 21:35:00 -0500

    This is my initial commit during class. File is a work in progress.

---
+++ hs628_tools/HS628_Project1.py
@@ -0,0 +1,38 @@
+Prompt Questions:
+    * What type of plot do you want to output?
+        - Overall linear regression
+        - Scatterplot
+        - Bar plots of variables
+        - Histogram
+        - Correlation plot
+        - Box plots
+
+    * What is the x axis variables and title?
+    * What is the y axis variable and title?
+    * What is the title of the graph?
+    * What color scheme?
+    * What variable names do you want for your legend?
+        - counts number of Xs and shows this:
+            - Var 1 title:
+            - Var 2 title:
+            - ... etc ...
+
+Functions:
+    - Each type of plot with x and y variable input answers coded in 
+    - Labeling the graph
+        - inputs from x and y titles, overall title, legend x titles
+        - color scheme
+    - Something that includedes the answer from Q2 so that it can come up in Q3 and Q8 
+
+## MAIN
+
+Q1= raw_input("What type of plot output? (Enter #)"+"\n"+"\n"+"1.Linear Regression"+"\n"+"2.Scatterplot"+"\n"+"3.Histogram"+"\n"+"4.Correlation Plot"+"\n"+"\n")
+Q2= input("How many independent variables do you want to use?")
+Q3= raw_input("Name independent variables:"+"\n"+"\n"+#function#)
+Q4= raw_input("What do you want to label the x axis?")
+Q5= raw_input("What do you want to label the y axis?")
+Q6= raw_input("What do you want to title the graph?")
+Q7 = raw_input("Choose color scheme:"+"\n"+"\n"#Look up schemes#)
+Q8 = raw_input("What do you want to label each independent variable in the legend/key?"+"\n"+"\n"+#function#)
+
+
