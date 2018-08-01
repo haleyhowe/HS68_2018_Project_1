1156ad870cdfdfff0757e57d5e91434618c925c5
haleyhowe <haleyhowell@Haleys-MacBook-Air-2.local>
2018-07-31 21:54:56 -0700

    Modified Docstrings to proper format and changed main into function

--- hs628_tools/Visualization.py
+++ hs628_tools/Visualization.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import pandas as pd
 import sys 
 import plotly.plotly as py
 import plotly.graph_objs as go
-#plotly.tools.set_credentials_file(username='hhowell', api_key='BRP1aHhfTHwkqnvalVQL')
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,mean_absolute_error, r2_score
@@ -14,48 +14,82 @@ from sklearn.metrics import mean_squared_error,mean_absolute_error, r2_score
 #from Test import data_split as ds
 
 
-###Bar Plot-Count
-#This function counts all instances in a variable and creates a barplot to display the results 
-#@param: The variable to be counted, the x_axis label, title of the graph
-#@return: A count bar plot of the variable inputted 
-def count_and_print(variable, x_label, title):
-    def count(variable):
+def count_and_print(variable_name, x_label, title):
+    """   
+    This function counts all instances in a variable and creates a barplot to 
+    display the results.
+    
+    Args: 
+        variable_name: The name of the variable that is displayed
+        x_label: The name of the variable displayed on the chart
+        title: The title of the chart 
+    
+    Returns: A count bar plot of every instance of that variable 
+    
+    """
+    
+    def count(x1):
+        """
+        This function counts every instance of the variable and stores it in a dictionary
+        
+        Args: 
+            x1: The variable to be counted 
+        Returns: 
+            A dictionary of the variable. The key is the instance to be counted and the value
+            is the count value of that instance 
+        """
         new_dic = {}
-        c = (len(variable)-1)
-        for index, value in enumerate(variable):
+        c = (len(x1)-1)
+        for index, value in enumerate(x1):
             count = 0
             if((value in new_dic) == True):
                 pass
             else:
-                for index2, value2 in enumerate(variable):
+                for index2, value2 in enumerate(x1):
                     if value == value2:
                         count = count + 1
-                    if index2 == (len(variable)-1):
+                    if index2 == (len(x1)-1):
                         new_dic[value] =  count
         return new_dic
-
-    variable = variable
-
-    count_dictionary = count(variable)
+    
+    count_dictionary = count(variable_name)
     from collections import OrderedDict
     from operator import itemgetter
 
     count_ordered = (OrderedDict(sorted(count_dictionary.items(), key = itemgetter(0), reverse = False)))
 
-    def key_list(list):
+    def key_list(ordered_dictionary):
+        """
+        This function takes in a dictionary and returns a list of the keys only 
+        
+        Args:
+            ordered_dictionary: An ordered dictionary of the keys and values needed
+            to be extracted
+        Returns: 
+            keys_list: A list of the keys within that dictionary 
+        """
         keys_list= []
-        for key in list.keys():
+        for key in ordered_dictionary.keys():
             keys_list.append(key)
         return keys_list
 
-    def value_list(list):
+    def value_list(ordered_dictionary):
+        """
+        This function takes in a dictionary and returns a list of the values only 
+        
+        Args:
+            ordered_dictionary: An ordered dictionary of the keys and values needed
+            to be extracted
+        Returns: 
+            values_list: A list of the values within that dictionary 
+        """
         values_list= []
-        for value in list.values():
+        for value in ordered_dictionary.values():
             values_list.append(value)
         return values_list
+    
     keys = key_list(count_ordered)
     values = value_list(count_ordered)
-#Plot the variable
     f1 = (go.Bar(
                 x=keys,
                 y=values
@@ -81,18 +115,29 @@ def count_and_print(variable, x_label, title):
             )
     )
 
-    
     data = [f1]
     fig = go.Figure(data=data,layout=layout)
     return py.iplot(fig, filename='basic-bar')
 
 
-###Scatter Plot 
-#This function outputs a scatter plot with respects to user input. The user can specify
-#if they want to color each data point by a categorical variable
-#@param: x variable, y variable, x and y axis labels and the title of the plot
-#@returns: a scatter plot
 def scatter(x,y,colorby,xlabel,ylabel,title,new_data):
+    """
+    This function outputs a scatter plot with respects to user input. The user can specify
+    if they want to color each data point by a categorical variable.
+    
+    Args: 
+        x: The x variable
+        y: The y variable
+        colorby: categorical variable in which the data points will be displayed by color
+        xlabel: The label of the x-axis
+        ylabel: The label of the y-axis
+        title: The title of the chart
+        new_data: The data set being used 
+    
+    Returns: 
+        A scatter plot with respective labels.
+    
+    """
     trace1 = go.Scatter(
         x = x,
         y = y,
@@ -127,11 +172,20 @@ def scatter(x,y,colorby,xlabel,ylabel,title,new_data):
     fig = go.Figure(data=data,layout=layout)
     return py.iplot(fig, filename='scatter-plot-with-colorscale');
 
-###BoxPlot
-#This function creates a box plot of the two variables the user decides to examine
-#@param: the x and y vales, along with the labels and the title of the graph
-#@return: A box plot
-def boxplot(x,y, y_label, x_label, title):
+def boxplot(x,y, x_label, y_label, title):
+    """
+    This function creates a box plot of the two variables the user decides to examine
+    Args: 
+        x: The x variable
+        y: The y variable
+        x_label: The label of the x-axis
+        y_label: The label of the y-axis
+        title: The title of the chart
+    
+    Returns: 
+        A box plot with respective labels.
+    
+    """
     data = [go.Box(x= x,
             y= y)]
 
@@ -160,6 +214,7 @@ def model_metrics(X_train, X_test, y_train, y_test):
             y_train: dataframe with training set split of the dependent variable
             X_test: dataframe with the testing set split of all the independent variables
             y_test: dataframe with the testing set split of the dependent variable
+        Returns: The metrics of the linear regression ran 
             
     """
     reg_obj = linear_model.LinearRegression()
@@ -190,6 +245,8 @@ def plot_model(X_train, X_test, y_train, y_test, Q2, Q3, Q4, Q5, Q6, Q7):
             y_train: dataframe with training set split of the dependent variable
             X_test: dataframe with the testing set split of all the independent variables
             y_test: dataframe with the testing set split of the dependent variable
+        
+        Returns: A graphical representation of the linear model outputted
             
     """
     reg_obj = linear_model.LinearRegression()
@@ -231,8 +288,21 @@ def plot_model(X_train, X_test, y_train, y_test, Q2, Q3, Q4, Q5, Q6, Q7):
     return py.iplot(fig)
 
 
-def question(data2,X_train,X_test,y_train,y_test, Q1):
-    if Q1 == '2':
+def question(data2,X_train,X_test,y_train,y_test, plot_type):
+    """
+    This function takes in user input and runs whatever plot or visualization tool entered
+    
+    Args: 
+        data2: The data set being used
+        X_train: x training set
+        X_test: x testing set 
+        y_train: y training set
+        y_test: y testing set
+        plot_type: The type of plot the user wishes to see prompted from question() function
+    
+    Returns: The plot requested by the user 
+    """
+    if plot_type == '2':
         x1 = input("Please enter X variable:")
         x = data2[x1]
         x2 = input("Please enter X2 variable (for comparison):")
@@ -241,17 +311,17 @@ def question(data2,X_train,X_test,y_train,y_test, Q1):
         x_lab = input("Please enter the label for the X-Axis:")
         y_lab = input("Please enter the label for the Y-Axis:")
         title = input("Please enter the title of the graph:")
-        return scatter(x,xx, y, x_lab, y_lab, title,data2);
+        return scatter(x,y, xx, x_lab, y_lab, title,data2);
 
-    if Q1 == '1':
+    if plot_type == '1':
         x1 = input("Please enter a variable:")
-        x = data2[x1]
+        #x = data2[x1]
         x_lab = input("Please enter the label for the X-Axis:")
         title = input("Please enter the title of your graph:")
-        return count_and_print(x, x_lab, title);
+        return count_and_print(data2[x1], x_lab, title);
 
 
-    if Q1== '3':
+    if plot_type == '3':
         x = data2[input("Please enter X variable name:")]
         y = data2[input("Please enter Y variable name:")]
         x_lab = input("Please enter the label for the X-Axis:")
@@ -259,10 +329,10 @@ def question(data2,X_train,X_test,y_train,y_test, Q1):
         title = input("Please enter the title of your graph:")
         return boxplot(x,y,x_lab,y_lab,title);
     
-    if Q1 == '4':
+    if plot_type == '4':
         return model_metrics(X_train, X_test, y_train, y_test);
     
-    if Q1 == '5':
+    if plot_type == '5':
         Q2= input("What do you want to title the graph?"+"\n")
         Q3 = input("What is the predictor(dependent) variable name?:"+"\n")
         Q4 = input("Name of x axis:")
@@ -272,23 +342,31 @@ def question(data2,X_train,X_test,y_train,y_test, Q1):
         model_metrics(X_train, X_test, y_train, y_test)
         return plot_model(X_train, X_test, y_train, y_test,Q2,Q3,Q4,Q5,Q6,Q7)
 
-##### MAIN #####
+def prompt_user():
+    """
+    This function prompts the user for their information on the plot they wish to see
+    and returns the respective plot 
+    """
+    
+    Q1= input("What type of plot output? (Enter #)"+"\n"+"\n"+"1.Countplot"+"\n"+"2.Scatterplot"+"\n"+"3.Boxplot"+"\n"+"4.Simple Linear Regression Model"+"\n"+"5.Multiple Linear Regression Model"+"\n")
+    question(data,Xtrain,Xtest,ytrain,ytest, Q1)
 
-##Note these four lines of code are meant to test this file 
 #data = pd.read_csv('data.csv')
 #sub_data = data.iloc[1:10,1:10]
 #X = sub_data.iloc[:,0:3]
 #Y = sub_data['readmission_30d']
 
-Xtrain, Xtest, ytrain, ytest = train_test_split(X,Y, test_size = .5) #This assumes that Issue #11 Test.py module works and outputs the test ratio. 
-Xtrain = np.array(Xtrain)
-ytrain = np.array(ytrain)
-Xtest = np.array(Xtest)
-ytest = np.array(ytest)
-
-Q1= input("What type of plot output? (Enter #)"+"\n"+"\n"+"1.Countplot"+"\n"+"2.Scatterplot"+"\n"+"3.Boxplot"+"\n"+"4.Simple Linear Regression Model"+"\n"+"5.Multiple Linear Regression Model"+"\n")
-question(data,Xtrain,Xtest,ytrain,ytest, Q1)
+#Xtrain, Xtest, ytrain, ytest = train_test_split(X,Y, test_size = .5) #This assumes that Issue #11 Test.py module works and outputs the test ratio. 
+#Xtrain = np.array(Xtrain)
+#ytrain = np.array(ytrain)
+#Xtest = np.array(Xtest)
+#ytest = np.array(ytest)
+#prompt_user()
 
 
- 
+#count_and_print(data['readmission_30d'], "hey", "whats")
+
+
+
+
 
